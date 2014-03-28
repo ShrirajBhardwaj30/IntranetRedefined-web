bfb62803d5e6849a4ba0a4f8e9fa12ebdc2ee174
Kunal Khandelwal <k.khandelwal@iitg.ernet.in>
2014-03-27 21:24:34 -0400

    backup_1_28/3

---
+++ social_data/easytheme/cssinc.php
@@ -0,0 +1,175 @@
+body {background: url('<?php echo elgg_get_site_url(); ?>mod/easytheme/graphics/bkgr.gif');}
+
+.elgg-button-submit {
+	color: white;
+	text-shadow: 1px 1px 0px black;
+	text-decoration: none;
+	border: 1px solid #000;
+	background: #a95e27 url(<?php echo elgg_get_site_url(); ?>_graphics/button_graduation.png) repeat-x left 10px;
+}
+
+.elgg-button-submit:hover {
+	border-color: #000;
+	text-decoration: none;
+	color: white;
+	background:  #2f5980 url(<?php echo elgg_get_site_url(); ?>_graphics/button_graduation.png) repeat-x left 10px;
+}
+
+.elgg-breadcrumbs > li > a:hover {
+	color: #2f5980;
+	text-decoration: underline;
+}
+
+.elgg-menu-site-more > li > a:hover {
+	background: #2f5980; 
+	color: white;
+}
+
+.elgg-menu-page a:hover {
+	background-color: #2f5980; 
+	color: white;
+	text-decoration: none;
+}
+
+.elgg-menu-owner-block li a:hover {
+	background-color: #2f5980; 
+	color: white;
+	text-decoration: none;
+}
+
+a {
+	color: #a95e27;
+}
+
+h1, h2, h3, h4, h5, h6 {
+	font-weight: bold;
+	color: #2f5980; 
+}
+
+.elgg-heading-basic {
+	color: #2f5980;
+	font-size: 1.2em;
+	font-weight: bold;
+}
+
+.elgg-loud {
+	color: #2f5980; 
+}
+
+
+
+.elgg-menu-page li.elgg-state-selected > a {
+	background-color: #2f5980; 
+	color: white;
+}
+
+.elgg-heading-site, .elgg-heading-site:hover {
+	font-size: 2em;
+	line-height: 1.4em;
+	color: #000;
+	font-style: italic;
+	font-family: Georgia, times, serif;
+	text-shadow: 1px 2px 4px #333333;
+	text-decoration: none;
+   background: #fff;
+	padding: 20px;
+	padding-left: 15px;
+}
+
+.elgg-page-default .elgg-page-header  .elgg-inner {
+	width: 990px;
+	height: 295px;
+   background: url('<?php echo elgg_get_site_url(); ?>mod/easytheme/graphics/headimg.jpg');
+}
+
+.elgg-page-default {
+	width: 990px;
+	margin: 0px auto; 
+	border-right: 1px solid #000000;
+        background: #ffffff;
+        border-left: 1px solid #000000;
+	-moz-box-shadow: 0 0 10px #888;
+	-webkit-box-shadow: 0 0 10px #888;
+	box-shadow: 0 0 10px #181a2f;
+}
+
+.elgg-heading-site, .elgg-heading-site:hover {
+	-webkit-border-bottom-right-radius: 24px;
+	-moz-border-bottom-right-radius: 24px;
+	border-bottom-right-radius: 24px;
+	opacity: 0.6;
+	-ms-filter:"progid:DXImageTransform.Microsoft.Alpha(Opacity=60)";
+        filter: alpha(opacity=60);
+}
+
+.elgg-page-footer {
+	height: 100px;
+}
+
+.elgg-page-footer {
+       color: #999; 
+       background: #181a2f;   
+}
+.elgg-page-footer a:link {
+	color: #ccc;
+}
+      
+.elgg-page-footer a:hover {
+	color: #666;
+}
+
+.elgg-menu-item-report-this{
+        margin-left: 10px;
+	margin-top: 5px;
+}
+
+#login-dropdown {
+	position: absolute;
+	top: 10px;
+	right: 0;
+	z-index: 100;  
+	margin-right: 10px;          
+}
+
+
+.elgg-page-default {
+	min-width: 990px;
+}
+
+.elgg-page-default .elgg-page-body > .elgg-inner {
+	width: 990px;
+	margin: 0 auto;        
+	
+}
+
+.elgg-page-default .elgg-page-footer > .elgg-inner {
+	width: 990px; 
+	margin: 0 auto;
+	padding: 5px 0;
+	border-top: 1px solid #DEDEDE;
+	
+}
+
+.elgg-page-header .elgg-search {
+        margin-top: 206px;
+	margin-bottom: 2px;
+        margin-right: 5px;
+	height: 23px;
+	position: absolute;
+	right: 0;       
+}
+
+.elgg-menu-item-powered {
+visibility: hidden;
+}
+
+.elgg-menu-footer-default {
+	float: right;
+	padding-right: 10px;
+}
+
+.elgg-menu-site-default{
+	background: #181a2f; 
+	padding-top: 5px; 
+	width: 100%;
+}
